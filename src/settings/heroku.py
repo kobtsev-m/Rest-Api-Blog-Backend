1f6bd3656243017b61c0828056bbaf9ade29bf13
kobtsev-m <kobtsev.x@gmail.com>
2021-03-03 16:03:15 +0700

    Heroku logging settings

--- src/settings/heroku.py
+++ src/settings/heroku.py
@@ -20,4 +20,4 @@ AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 
-django_heroku.settings(locals())
+django_heroku.settings(locals(), logging=False)
